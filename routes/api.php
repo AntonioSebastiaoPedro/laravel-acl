4cd8c45185d95b927088425da1186d2d2551aba9
António Sebastião Pedro <antoniosebastiaopedro19@gmail.com>
2023-12-25 03:52:26 +0100

    Renomeia as totas de autenticação

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\{PermissionController, UserController};
 use App\Http\Controllers\Auth\AuthApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth');
+Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth.login');
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/me', [AuthApiController::class, 'me'])->name('auth.me');
-    Route::post('/logout', [AuthApiController::class, 'logout'])->name('logout');
+    Route::post('/logout', [AuthApiController::class, 'logout'])->name('auth.logout');
 
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
